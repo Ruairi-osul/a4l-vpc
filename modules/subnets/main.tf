c19f933bcebe7ba7e6bb49ea75fd9f58611c83e2
Ruairi-osul <ruairi.osullivan@univ.ox.ac.uk>
2024-02-27 11:30:09 -0500

    Added AZ and SubnetType tags to Subnets

--- modules/subnets/main.tf
+++ modules/subnets/main.tf
@@ -22,7 +22,9 @@ resource "aws_subnet" "a4l_subnet_public" {
   map_public_ip_on_launch         = true
   assign_ipv6_address_on_creation = true
   tags = {
-    Name = "a4l_subnet_${var.azs[count.index % local.num_azs]}_${var.public_subnets[count.index % local.num_subnets_public]}"
+    Name       = "a4l_subnet_${var.azs[count.index % local.num_azs]}_${var.public_subnets[count.index % local.num_subnets_public]}"
+    AZ         = var.azs[count.index % local.num_azs]
+    SubnetType = var.public_subnets[count.index % local.num_subnets_public]
   }
 }
 
@@ -38,6 +40,8 @@ resource "aws_subnet" "a4l_subnet_private" {
   )
   availability_zone = var.azs[count.index % local.num_azs]
   tags = {
-    Name = "a4l_subnet_${element(var.azs, count.index % length(var.azs))}_${element(var.private_subnets, floor(count.index / length(var.azs)))}"
+    Name       = "a4l_subnet_${element(var.azs, count.index % length(var.azs))}_${element(var.private_subnets, floor(count.index / length(var.azs)))}"
+    AZ         = var.azs[count.index % local.num_azs]
+    SubnetType = var.private_subnets[count.index % local.num_subnets_private]
   }
 }
